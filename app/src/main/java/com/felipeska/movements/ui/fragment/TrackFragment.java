55e5da8dcaf3b8c995c530a90ec01a949fac6c8a
felipeska <felipeskabarragan@gmail.com>
2015-02-26 21:25:30 -0500

    inject view TrackFragment

--- app/src/main/java/com/felipeska/movements/ui/fragment/TrackFragment.java
+++ app/src/main/java/com/felipeska/movements/ui/fragment/TrackFragment.java
@@ -5,13 +5,37 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.felipeska.movements.R;
+import com.todddavies.components.progressbar.ProgressWheel;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+import butterknife.OnClick;
 
 /**
  * @author felipeska
  */
 public class TrackFragment extends Fragment {
+
+  @InjectView(R.id.latitude)
+  TextView mTextViewLatitude;
+  @InjectView(R.id.longitude)
+  TextView mTextViewLongitude;
+  @InjectView(R.id.speed)
+  TextView mTextViewSpeed;
+
+  @InjectView(R.id.pw_spinner)
+  ProgressWheel mProgressWheel;
+
+  @OnClick(R.id.buttonStart)
+  void startTracking() {
+  }
+
+  @OnClick(R.id.buttonStop)
+  void stopTracking() {
+  }
 
   public TrackFragment() {
   }
@@ -20,6 +44,31 @@ public class TrackFragment extends Fragment {
   public View onCreateView(LayoutInflater inflater, ViewGroup container,
                            Bundle savedInstanceState) {
     View rootView = inflater.inflate(R.layout.fragment_track, container, false);
+    ButterKnife.inject(this, rootView);
     return rootView;
   }
+
+  @Override
+  public void onActivityCreated(Bundle savedInstanceState) {
+    super.onActivityCreated(savedInstanceState);
+  }
+
+  @Override
+  public void onDestroyView() {
+    super.onDestroyView();
+    ButterKnife.reset(this);
+  }
+
+  @Override
+  public void onResume() {
+    super.onResume();
+    mProgressWheel.spin();
+  }
+
+  @Override
+  public void onPause() {
+    super.onPause();
+    mProgressWheel.stopSpinning();
+  }
+
 }
